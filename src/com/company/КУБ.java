ac38e9a62fcce11b28a290875251d712ac76c646
vlladd <E11ori0n@bk.ru>
2017-11-09 21:00:55 +1000

    Initial commit

---
+++ src/com/company/КУБ.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class КУБ {
+
+    int x,y,a,b;
+    String colir;
+
+        public КУБ () {}
+
+
+            public КУБ (int a, int b){}
+    int sqгare(){
+        return (this.a * this.b);
+    }
+    int area (){
+
+        return  (2 * this.a + 2 * this.b);
+    }
+}
